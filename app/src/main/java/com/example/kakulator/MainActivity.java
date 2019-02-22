a25f51fe1844d0dc3aa566c0a9b95cbcb4ca9384
Danii Sherbina <stronglav.cf@gmail.com>
2019-02-23 01:34:26 +0700

    test connect

--- app/src/main/java/com/example/kakulator/MainActivity.java
+++ app/src/main/java/com/example/kakulator/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a =5;
+        String s1 = "";
     }
 }
